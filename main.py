2f2ecb7608784d3a5f1e0cc2b77792b985400f7e
Peyman Majidi <Peyman.majidi@gmail.com>
2019-03-21 17:05:19 +0330

    color bar

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 from tkinter import *
 from tkinter import ttk
 
-fillcolor = 'black'
+fillcolor = 'red'
     
 def draw(e):
     global pre
-    canvas.create_line(pre.x, pre.y, e.x, e.y, width = 10, fill = fillcolor)
+    canvas.create_line(pre.x-2, pre.y-2, e.x+2, e.y+2, width = 15, fill = fillcolor)
+    pre = e
+
+def setcolor(color):
+    global fillcolor
+    fillcolor = color
+    print('fillcolor switch to: ' + fillcolor)
+
+def setBackColor():
+    canvas.config(background = fillcolor)
+
+
+def eraser(e):
+    global pre
+    canvas.create_line(pre.x-3, pre.y-3, e.x+3, e.y+3, width = 30, fill = 'white')
     pre = e
 
 def point(e):
@@ -14,20 +28,43 @@ def point(e):
 
 def command(e):
     if e.char == 'r':
-        fillcolor = 'red'
+        color = 'red'
     elif e.char == 'b':
-        fillcolor = 'black'
+        color = 'black'
     elif e.char == 'g':
-        fillcolor = 'green'
+        color = 'green'
     elif e.char == 'y':
-        fillcolor = 'yellow'
-
+        color = 'yellow'
+    setcolor(color)
 
 
 root = Tk()
+col = 1
+Button(root, text='قرمز',command= lambda: setcolor('red'), background = 'red').grid(row=0,column = col)
+col +=1
+Button(root, text='آبی',command= lambda: setcolor( 'blue'), background =  'blue').grid(row=0,column = col)
+col +=1
+Button(root, text='سبز',command= lambda: setcolor('green'), background = 'green').grid(row=0,column = col)
+col +=1
+Button(root, text='قهوه ای',command= lambda: setcolor('brown'), background = 'brown').grid(row=0,column = col)
+col +=1
+Button(root, text='زرد',command= lambda: setcolor('yellow'), background = 'yellow').grid(row=0,column = col)
+col +=1
+Button(root, text='بنفش',command= lambda: setcolor('violet'), background = 'violet').grid(row=0,column = col)
+col +=1
+Button(root, text='نارنجی',command= lambda: setcolor('orange'), background = 'orange').grid(row=0,column = col)
+col +=1
+Button(root, text='خاکستری',command= lambda: setcolor('gray'), background = 'gray').grid(row=0,column = col)
+col +=1
+Button(root, text='سفید',command= lambda: setcolor('white'), background = 'white').grid(row=0,column = col)
+col +=1
+Button(root, text='مشکی',command= lambda: setcolor('black'), background = 'black', foreground = 'white').grid(row=0,column = col)
+
 canvas = Canvas(root, width= 800, height = 600 , background = 'white')
-canvas.pack()
+canvas.grid(row = 1, column = 0, columnspan = 11)
 canvas.bind('<ButtonPress>', point)
 canvas.bind('<B1-Motion>',draw)
-root.bind('<Keypress>',command)
+canvas.bind('<B3-Motion>',eraser)
+canvas.bind('<B2-Motion>', lambda e: setBackColor())
+root.bind('<KeyPress>',command)
 root.mainloop()
